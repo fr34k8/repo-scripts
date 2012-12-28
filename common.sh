ea7c2f3f97f7ef89ed1276e06c135ce6fa83032b
Benedikt Böhm <bb@xnull.de>
2012-12-28 09:39:55 +0100

    add support for blacklist and atom expansion via profiles

--- common.sh
+++ common.sh
@@ -2,6 +2,7 @@ source "/etc/init.d/functions.sh"
 
 GENTOO_CACHE="${GENTOO_CACHE:-${HOME}/cache/gentoo-portage}"
 REPO_NAME=$(<"${TOPDIR}"/profiles/repo_name)
+BLACKLIST=$(<"${TOPDIR}"/profiles/releases/zentoo/eupdate.blacklist)
 
 die() {
 	eerror "$@"
@@ -16,8 +17,27 @@ geix() {
 		"$@"
 }
 
+fcopy() {
+	for file in "$@"; do
+		mkdir -p "$(dirname "${TOPDIR}/${file}")"
+		einfo "  - ${file}"
+		cp "${GENTOO_CACHE}/${file}" "${TOPDIR}/${file}"
+	done
+}
+
 is_overlay() {
 	[[ $(<"${TOPDIR}"/profiles/repo_name) =~ "^zentoo" ]]
+}
+
+is_blacklisted() {
+	for a in ${BLACKLIST}; do
+		if [[ ${1} == ${a} ]]; then
+			eerror "${1} is blacklisted, skipping copy ..."
+			return 0
+		fi
+	done
+
+	return 1
 }
 
 setup_portdir() {
